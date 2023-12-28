f9eed9728b0a16e53512fdcd5b3d5a01b5d70cf3
raphael <raphaelqueiroz775@gmail.com>
2023-12-28 18:05:26 -0300

    enemy dying and falling when shot

--- projeto wendell/Assets/Scripts/Enemy.cs
+++ projeto wendell/Assets/Scripts/Enemy.cs
@@ -5,22 +5,59 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [Header("Variables")]
-    public float speed;
+    public float movSpeed;
+    //public float fallSpeed;
+    [Header("Others")]
+    public Transform[] patrolPoints;
+    public int destination;
+    //public Transform deathPoint;
+    public Rigidbody2D rig;
+    [Header("Booleans")]
+    public bool isDead = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(destination == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, movSpeed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, patrolPoints[0].position) == 0){
+                destination = 1;
+            }
+        }
+        if (destination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, movSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) == 0){
+                destination = 2;
+            }
+        }
         
+
     }
 
     void OnMouseDown()
     {
+        movSpeed = 0;
+        isDead = true;
+
+        if (isDead)
+        {
+            StartCoroutine(Death());
+        }
+    }
+
+    IEnumerator Death()
+    {
+        yield return new WaitForSeconds(2);
+        rig.gravityScale = 1;
+        yield return new WaitForSeconds(6);
         Destroy(gameObject);
     }
 }
