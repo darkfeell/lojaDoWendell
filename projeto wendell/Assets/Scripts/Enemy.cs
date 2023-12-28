da916e0b0b16215e0e5c33dbe9ecca2073cc0419
raphael <raphaelqueiroz775@gmail.com>
2023-12-28 18:35:17 -0300

    some small changes

--- projeto wendell/Assets/Scripts/Enemy.cs
+++ projeto wendell/Assets/Scripts/Enemy.cs
@@ -24,21 +24,8 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(destination == 0)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, movSpeed * Time.deltaTime);
-            if(Vector2.Distance(transform.position, patrolPoints[0].position) == 0){
-                destination = 1;
-            }
-        }
-        if (destination == 1)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, movSpeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, patrolPoints[1].position) == 0){
-                destination = 2;
-            }
-        }
-        
+        MoveBetweenPoints();
+
 
     }
 
@@ -59,5 +46,48 @@ public class Enemy : MonoBehaviour
         rig.gravityScale = 1;
         yield return new WaitForSeconds(6);
         Destroy(gameObject);
+    }
+    void MoveBetweenPoints()
+    {
+        if (destination == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, movSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) == 0)
+            {
+                destination = 1;
+            }
+        }
+        if (destination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, movSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) == 0)
+            {
+                destination = 2;
+            }
+        }
+        if (destination == 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[2].position, movSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[2].position) == 0)
+            {
+                destination = 3;
+            }
+        }
+        if (destination == 3)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[3].position, movSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[3].position) == 0)
+            {
+                destination = 4;
+            }
+        }
+        if (destination == 4)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[4].position, movSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[4].position) == 0)
+            {
+                destination = 5;
+            }
+        }
     }
 }
